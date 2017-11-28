85bb5f0b2201ea4c1c81c901def81b6f4bf66cd9
suxingdian1992 <suxingdian@live.com>
2017-11-28 08:40:03 +0800

    修改下注释的位置

--- ServeletDemo/src/com/httpservlet/demo/CookieClassServlet.java
+++ ServeletDemo/src/com/httpservlet/demo/CookieClassServlet.java
@@ -15,8 +15,7 @@ import javax.servlet.http.HttpServletResponse;
  * 1、同一应用下cookie传递控制当前页面最多能显示多少条记录，都在cookietest类中统一设定
  * 2017年11月27日
  */
-@WebServlet(name = "CookieClassServlet", 
-        urlPatterns = { "/cookieClass" })
+@WebServlet(name = "CookieClassServlet", urlPatterns = { "/cookieClass" })
 public class CookieClassServlet extends HttpServlet {
     private static final long serialVersionUID = 837369L;
     
